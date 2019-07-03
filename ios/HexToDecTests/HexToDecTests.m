dc295e9987d8b8b197bf59a78743ab49e81ab822
KurzGedanke <kurzgedanke@pm.me>
2019-07-03 21:56:06 +0200

    app rename

--- ios/HexToDecTests/HexToDecTests.m
+++ ios/HexToDecTests/HexToDecTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface DecimalIPTests : XCTestCase
+@interface HexToDecTests : XCTestCase
 
 @end
 
-@implementation DecimalIPTests
+@implementation HexToDecTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
